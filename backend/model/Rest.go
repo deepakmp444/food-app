264d08d8ff9e9fa451458331baa9ce247a011f98
“deepakmp444” <deepakmp444@gmail.com>
2024-05-25 10:07:06 +0530

    changes

---
+++ backend/model/Rest.go
@@ -0,0 +1,16 @@
+package model
+
+// Restaurant represents the structure of a restaurant document
+type Restaurant struct {
+    ID             string  `bson:"_id"`
+    Name           string  `bson:"restaurant_name"`
+    Type           string  `bson:"restaurant_type"`
+    Rating         float64 `bson:"rate"`
+    NumOfRatings   int     `bson:"num_of_ratings"`
+    AvgCost        int     `bson:"avg_cost"`
+    OnlineOrder    string  `bson:"online_order"`
+    TableBooking   string  `bson:"table_booking"`
+    Cuisines       string  `bson:"cuisines_type"`
+    Area           string  `bson:"area"`
+    LocalAddress   string  `bson:"local_address"`
+}
